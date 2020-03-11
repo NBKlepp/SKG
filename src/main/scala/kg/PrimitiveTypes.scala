76bc064a384eb5d41ea313a42e5f8dd232958d03
Nicholas Klepp <nickbk@uga.edu>
2020-03-11 16:06:26 -0400

    Initial commit for the NodeTypeSubClass

---
+++ src/main/scala/kg/PrimitiveTypes.scala
@@ -0,0 +1,10 @@
+package kg
+
+import scala.collection.immutable.Vector
+import scalation.math.{Complex, Rational, Real}
+import scalation.math.StrO
+import scalation.math.StrO.StrNum
+import scalation.math.TimeO
+    
+type PrimitiveType = Real.type | Rational.type | StrO.type   | TimeO.type    | Complex.type | Int.type | Double.type | Long.type
+type Primitive     = Real      | Rational      | StrO.StrNum | TimeO.TimeNum | Complex      | Int      | Double      | Long
